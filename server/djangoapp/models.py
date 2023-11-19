d92f9110aa395fc5ca900f3665e5e8414e47e5eb
Your Name <you@example.com>
2023-11-19 09:27:11 -0500

    added review

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -31,31 +31,37 @@ class CarModel(models.Model):
 
 
 class CarDealer:
-
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
-        # Dealer address
         self.address = address
-        # Dealer city
         self.city = city
-        # Dealer Full Name
         self.full_name = full_name
-        # Dealer id
         self.id = id
-        # Location lat
         self.lat = lat
-        # Location long
         self.long = long
-        # Dealer short name
         self.short_name = short_name
-        # Dealer state
         self.st = st
-        # Dealer zip
         self.zip = zip
 
     def __str__(self):
         return "Dealer name: " + self.full_name
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class DealerReview:
+    def __init__(self,dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.id
+        self.name
+        self.dealership
+        self.review
+        self.purchase
+        self.purchase_date
+        self.car_make
+        self.car_model
+        self.car_year
+        self.sentiment
+
+    def __str__(self):
+        return "Dealer review: " + self.review 
+
+
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-from .restapis import get_dealers_from_cf
+from .restapis import get_dealers_from_cf, get_dealer_by_id, get_dealers_by_state, get_dealer_reviews_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -14,21 +14,16 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
 
 # Create an `about` view to render a static about page
 def about(request):
-    context = {}
-
-    return render(request, 'djangoapp/about.html', context)
-# ...
+    return render(request, 'djangoapp/about.html')
 
 
 # Create a `contact` view to return a static contact page
 def contact(request):
-    context = {}
-    return render(request, 'djangoapp/contact.html', context)
+    return render(request, 'djangoapp/contact.html')
+
 
 # Create a `login_request` view to handle sign in request
 def login_request(request):
@@ -46,11 +41,13 @@ def login_request(request):
     else:
             return render(request, 'djangoapp/user_login.html', context)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     print("Log out the user '{}'".format(request.user.username))
     logout(request)
     return redirect('djangoapp:index')
+
 
 # Create a `registration_request` view to handle sign up request
 def registration_request(request):
@@ -84,21 +81,35 @@ def registration_request(request):
         else:
             return render(request, 'djangoapp/registration.html', context)
 
+
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
+    context = {}
     if request.method == "GET":
-        url = "https://valerioceles-3000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        url = "https://valerioceles-3000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
         # Concat all dealer's short name
-        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        #dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        context['dealership_list'] = dealerships
         # Return a list of dealer short name
-        return HttpResponse(dealer_names)
+        return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
-# ...
-
+def get_dealer_details(request, dealer_id):
+    if request.method == "GET":
+        context = {}
+        dealer_url = "https://valerioceles-3000.theiadocker-2-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        dealer = get_dealer_from_cf_by_id(dealer_url, id=dealer_id)
+        context["dealer"] = dealer
+    
+        review_url = "https://e622ef87-6e23-4638-ae7f-c94671c7179f-bluemix.cloudantnosqldb.appdomain.cloud"
+        reviews = get_dealer_reviews_from_cf(review_url, id=dealer_id)
+        print(reviews)
+        context["reviews"] = reviews
+        
+        return render(request, 'djangoapp/dealer_details.html', context)
 # Create a `add_review` view to submit a review
 # def add_review(request, dealer_id):
 # ...
